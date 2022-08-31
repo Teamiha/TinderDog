008cc9627b757b7d26700c4795c685a401f4f385
Mikhail Svetlov <jegnum@gmail.com>
2022-08-31 23:58:10 +0300

    Add empty arrow button

--- TinderDog/MainView/MainViewController.swift
+++ TinderDog/MainView/MainViewController.swift
@@ -21,7 +21,7 @@ class MainViewController: UIViewController {
     
     //MARK: - Views
     
-     var dogImage: UIImageView = {
+     lazy var dogImage: UIImageView = {
         
         let image = UIImageView(image: UIImage(named: "testPicture"))
         image.translatesAutoresizingMaskIntoConstraints = false
@@ -30,11 +30,22 @@ class MainViewController: UIViewController {
         return image
     }()
     
+    lazy var nextButton: UIButton = {
+        let image = UIImage(systemName: "arrow.right.circle.fill")
+       
+        let button = UIButton()
+//        button.setTitle("Test", for: .normal)
+        button.setImage(UIImage(systemName: "arrow.right.circle.fill",  withConfiguration: UIImage.SymbolConfiguration(pointSize: 100)) , for: .normal)
+//        button.sizeThatFits(CGSize(width: 100, height: 100))
+        
+        return button
+    }()
     //MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(dogImage)
+        view.addSubview(nextButton)
         view.backgroundColor = .white
         setConstraints()
         configureNavigationBar()
@@ -44,17 +55,26 @@ class MainViewController: UIViewController {
 }
 
 
-
+//MARK: - Private Methods
 
 private extension MainViewController {
     
+//MARK: - Constraints
+    
     func setConstraints() {
         dogImage.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             dogImage.topAnchor.constraint(equalTo: view.topAnchor, constant: Constants.fromImageToTop),
             dogImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.imageLeadingSpace),
             dogImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.imageTrailingSpace)
+        ])
+        
+        NSLayoutConstraint.activate([
+            nextButton.topAnchor.constraint(equalTo: dogImage.bottomAnchor, constant: 50),
+            nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.imageLeadingSpace),
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.imageTrailingSpace)
         ])
     }
     
