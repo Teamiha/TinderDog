4e6f8ad9f3b78b0b4aaa9985d59af57d8fcf72a1
Mikhail Svetlov <jegnum@gmail.com>
2022-09-01 23:17:03 +0300

    add favorite and detail button

--- TinderDog/MainView/MainViewController.swift
+++ TinderDog/MainView/MainViewController.swift
@@ -15,6 +15,21 @@ class MainViewController: UIViewController {
         static let fromImageToTop: CGFloat = 50
         static let imageLeadingSpace: CGFloat = 40
         static let imageTrailingSpace: CGFloat = -40
+        
+        static let fromNextButtonTopToImageBottom: CGFloat = 10
+        static let nextButtonLeadingSpace: CGFloat = 130
+        static let nextButtonTrailingSpace: CGFloat = -130
+        static let nextButtonSize: CGFloat = 100
+        
+        static let fromDetailButtonTopToImageBottom: CGFloat = 25
+        static let detailButtonLeadingSpace: CGFloat = 40
+        static let detailButtonTrailingSpace: CGFloat = -270
+        static let detailButtonSize: CGFloat = 70
+        
+        static let favoriteButtonTopToImageBottom: CGFloat = 25
+        static let favoriteLeadingSpace: CGFloat = 270
+        static let favoriteTrailingSpace: CGFloat = -40
+        static let favoriteButtonSize: CGFloat = 70
     }
     
     
@@ -31,21 +46,50 @@ class MainViewController: UIViewController {
     }()
     
     lazy var nextButton: UIButton = {
-        let image = UIImage(systemName: "arrow.right.circle.fill")
-       
         let button = UIButton()
-//        button.setTitle("Test", for: .normal)
-        button.setImage(UIImage(systemName: "arrow.right.circle.fill",  withConfiguration: UIImage.SymbolConfiguration(pointSize: 100)) , for: .normal)
-//        button.sizeThatFits(CGSize(width: 100, height: 100))
+        let image = UIImage(systemName: "arrow.right.circle.fill",  withConfiguration: UIImage.SymbolConfiguration(pointSize: Constants.nextButtonSize))
+        let imagePressed = image!.withTintColor(.gray, renderingMode: .alwaysOriginal)
+        
+        button.tintColor = .black
+        
+        button.setImage(image, for: .normal)
+        button.setImage(imagePressed, for: .highlighted)
         
         return button
     }()
+    
+    lazy var favoriteButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(systemName: "heart.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: Constants.favoriteButtonSize))
+        
+        button.tintColor = .systemPink
+        
+        button.setImage(image, for: .normal)
+        
+        return button
+    }()
+    
+    lazy var detailButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(systemName: "list.bullet.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: Constants.detailButtonSize))
+        let imagePressed = image!.withTintColor(.gray, renderingMode: .alwaysOriginal)
+        
+        button.tintColor = .black
+        
+        button.setImage(image, for: .normal)
+        button.setImage(imagePressed, for: .highlighted)
+        
+        return button
+    }()
+    
+    
+    
+    
     //MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(dogImage)
-        view.addSubview(nextButton)
+        setupSubviews(dogImage, nextButton, favoriteButton, detailButton)
         view.backgroundColor = .white
         setConstraints()
         configureNavigationBar()
@@ -59,11 +103,19 @@ class MainViewController: UIViewController {
 
 private extension MainViewController {
     
+    func setupSubviews(_ subviews: UIView...) {
+            subviews.forEach { subview in
+                view.addSubview(subview)
+            }
+        }
+    
 //MARK: - Constraints
     
     func setConstraints() {
         dogImage.translatesAutoresizingMaskIntoConstraints = false
         nextButton.translatesAutoresizingMaskIntoConstraints = false
+        favoriteButton.translatesAutoresizingMaskIntoConstraints = false
+        detailButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             dogImage.topAnchor.constraint(equalTo: view.topAnchor, constant: Constants.fromImageToTop),
@@ -72,10 +124,23 @@ private extension MainViewController {
         ])
         
         NSLayoutConstraint.activate([
-            nextButton.topAnchor.constraint(equalTo: dogImage.bottomAnchor, constant: 50),
-            nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.imageLeadingSpace),
-            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.imageTrailingSpace)
+            nextButton.topAnchor.constraint(equalTo: dogImage.bottomAnchor, constant: Constants.fromNextButtonTopToImageBottom),
+            nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.nextButtonLeadingSpace),
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.nextButtonTrailingSpace)
         ])
+        
+        NSLayoutConstraint.activate([
+            favoriteButton.topAnchor.constraint(equalTo: dogImage.bottomAnchor, constant: Constants.favoriteButtonTopToImageBottom),
+            favoriteButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.favoriteLeadingSpace),
+            favoriteButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.favoriteTrailingSpace)
+         ])
+        
+        NSLayoutConstraint.activate([
+            detailButton.topAnchor.constraint(equalTo: dogImage.bottomAnchor, constant: Constants.fromDetailButtonTopToImageBottom),
+            detailButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.detailButtonLeadingSpace),
+            detailButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.detailButtonTrailingSpace)
+         ])
+            
     }
     
     func configureNavigationBar() {
